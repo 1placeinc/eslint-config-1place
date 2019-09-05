1ff902bd39f99a43f96824aa3e160f812aa5c4a6
Manuel Vila <mvila@3base.com>
2019-09-05 09:41:51 +0900

    Remove 'react-hooks' plugin

--- react.js
+++ react.js
@@ -4,7 +4,6 @@ const path = require('path');
 
 module.exports = {
   "extends": [path.join(__dirname, "index.js"), "plugin:react/recommended"],
-  "plugins": ["react-hooks"],
   "env": {
     "browser": true
   },
@@ -14,8 +13,6 @@ module.exports = {
     }
   },
   "rules": {
-    "react/no-unescaped-entities": 0,
-    "react-hooks/rules-of-hooks": 2,
-    "react-hooks/exhaustive-deps": 2
+    "react/no-unescaped-entities": 0
   }
 };
