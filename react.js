5e16b24baa7bb6e6d1edc53333bc3e42963798fc
Manuel Vila <mvila@3base.com>
2020-01-11 11:31:30 +0900

    Use eslint-config-prettier

--- react.js
+++ react.js
@@ -3,16 +3,20 @@
 const path = require('path');
 
 module.exports = {
-  "extends": [path.join(__dirname, "index.js"), "plugin:react/recommended"],
-  "env": {
-    "browser": true
+  extends: [
+    path.join(__dirname, 'index.js'),
+    'plugin:react/recommended',
+    require.resolve('eslint-config-prettier/react')
+  ],
+  env: {
+    browser: true
   },
-  "settings": {
-    "react": {
-      "version": "16"
+  settings: {
+    react: {
+      version: '16'
     }
   },
-  "rules": {
-    "react/no-unescaped-entities": 0
+  rules: {
+    'react/no-unescaped-entities': 0
   }
 };
